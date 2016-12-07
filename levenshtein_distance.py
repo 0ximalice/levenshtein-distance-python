ec0f33ec734c5e2d111fb21604bbbf992f35c8ca
Kiettiphong Manovisut <manovisut.ktp@gmail.com>
2016-12-08 00:08:47 +0700

    add files

---
+++ levenshtein_distance.py
@@ -0,0 +1,31 @@
+import numpy
+
+def levenshteinDistance(s1, s2):
+    if len(s1) > len(s2):
+        s1, s2 = s2, s1
+
+    distances = range(len(s1) + 1)
+    for i2, c2 in enumerate(s2):
+        distances_ = [i2+1]
+        for i1, c1 in enumerate(s1):
+            if c1 == c2:
+                distances_.append(distances[i1])
+            else:
+                distances_.append(1 + min((distances[i1], distances[i1 + 1], distances_[-1])))
+        distances = distances_
+    return distances[-1]
+
+print('------ Calculating ------')
+msg = ''
+lab = ''
+with open('text.lab') as f:
+    lab = f.read().replace('\n', '')
+
+with open('text.res') as f:
+    msg = f.read().replace('\n', '')
+
+#result = wer(list(lab), list(msg), False)
+result = levenshteinDistance(lab, msg)
+bigger = max(len(lab), len(msg))
+result = ((bigger - result) / bigger) * 100
+print('Accuracy : %.2f%%'%(result))
